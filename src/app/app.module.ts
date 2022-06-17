36da962fbf1f24dac2f3284efb292a6b3f048b69
TamaraZolnajic <zolnajic.tamara@gmail.com>
2022-06-17 18:57:01 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { CartComponent } from './cart/cart.component';
+import { SpecialsComponent } from './specials/specials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent,
+    AboutUsComponent,
+    MenuComponent,
+    CartComponent,
+    SpecialsComponent,
+
   ],
   imports: [
     BrowserModule,
