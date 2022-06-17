616ba308962fb9b3c9bc6f67e683b59c6d5610d3
TamaraZolnajic <zolnajic.tamara@gmail.com>
2022-06-18 01:11:11 +0200

    finished cr6

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { products } from '../products';
 import { IProducts } from '../IProducs';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
-  product : IProducts = {} as IProducts;
+export class ProductDetailsComponent implements OnInit, DoCheck {
+  products1 : IProducts = {} as IProducts;
   id : number = 0;
-  constructor( private route : ActivatedRoute) { }
-
+  count: number = 0;
+  products2 : IProducts [] = products
+  constructor( private route : ActivatedRoute, private CS: CartService) { }
+  addToCart1(){
+  this.CS.addToCart(this.products1);
+  }
   ngOnInit(): void {
     this.route.params.subscribe((params:Params) => {
       this.id = +params ["Id"];
-      this.product = products[this.id];
+      this.products1 = products[this.id];
     })
   }
-
+ ngDoCheck(): void {
+   this.count =this.CS.itemsLength();
+ }
 }
